16d132f8afc8b7486bda8150a6f27b47de7a87a5
Dennis Rübner <dennis.ruebner@gmail.com>
2017-05-21 14:04:13 +0200

    Begin of LevelManager added, need to get to know new Scenemanager

---
+++ GoblettMampfer/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start()
+    {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            //Debug.Log("Autoload Level Disabled!");
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        
+    }
+}
