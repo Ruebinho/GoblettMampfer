3aee42c60aa57bcc166903f152e1d247fe98ba23
Dennis Rübner <dennis.ruebner@gmail.com>
2017-06-08 20:26:06 +0200

    Menu added, now Bug with restarting the game after returning to Menu

--- GoblettMampfer/Assets/Scripts/LevelManager.cs
+++ GoblettMampfer/Assets/Scripts/LevelManager.cs
@@ -33,6 +33,6 @@ public class LevelManager : MonoBehaviour {
 
     public void LoadNextLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
